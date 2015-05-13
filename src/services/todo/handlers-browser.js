67d788338aa57d8945746aad051d1d9c26b67c46
Patrick Steele-Idem <pnidem@gmail.com>
2015-05-13 13:17:57 -0600

    Additional code documentation

---
+++ src/services/todo/handlers-browser.js
@@ -0,0 +1,6 @@
+// This file is intentionally left empty.
+//
+// Handlers do not have a client-side implement since service handlers are
+// implemented on the server. We use the "browser" field in this directory's
+// package.json to remap the "./handlers.js" module to an empty implementation
+// for the browser.
