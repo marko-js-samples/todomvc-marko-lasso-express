db4c270a573f78fda3e599b15df6ce1319fffafa
Patrick Steele-Idem <pnidem@gmail.com>
2015-05-19 14:50:41 -0600

    Don't do initialServerSync() when running tests

--- src/app/todo/index-browser.js
+++ src/app/todo/index-browser.js
@@ -12,6 +12,13 @@ module.exports = window.app = app;
 
 window.addEventListener('load', function load(event){
     window.removeEventListener('load', load);
-    app.initialServerSync(false);
+
+    // Don't do the initial server sync if we are being loaded in
+    // a test environment that uses a local file system file
+    var url = document.location.toString();
+    if (url.indexOf('http') !== -1) {
+        app.initialServerSync(false);
+    }
+
 });
 
